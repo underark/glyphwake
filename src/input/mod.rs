c65c16b9d6b571954fde5b57f230d0e36acef836
alex <peter.alex.morris@gmail.com>
2026-05-14 22:00:17 +0900

    Added method to input listener to get keyboard mapping info

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -3,30 +3,60 @@ use std::thread;
 
 use x11rb::protocol::Event;
 use x11rb::protocol::xinput::{Device, DeviceId, EventMask, XIEventMask, xi_select_events};
+use x11rb::protocol::xproto::ConnectionExt;
 use x11rb::{connection::Connection, rust_connection::RustConnection};
 
 pub struct XInputListener {
     conn: RustConnection,
     root: u32,
+    kbd: KbdData,
+}
+
+struct KbdData {
+    first_key: u8,
+    max_key: u8,
+    keysyms: Vec<u32>,
+    keysyms_per: u8,
 }
 
 impl XInputListener {
     fn new() -> Self {
         let (conn, num) = x11rb::connect(None).unwrap();
-        let screen = &conn.setup().roots[num];
+        let setup = conn.setup();
+        let screen = &setup.roots[num];
         let root = screen.root;
-        let listener = Self { conn, root };
+        let kbd = KbdData {
+            first_key: setup.min_keycode,
+            max_key: setup.max_keycode,
+            keysyms: vec![],
+            keysyms_per: 0,
+        };
+        let mut listener = Self { conn, root, kbd };
         listener.init();
         listener
     }
 
-    fn init(&self) {
+    fn init(&mut self) {
         let mask = EventMask {
             deviceid: DeviceId::from(Device::ALL),
             mask: vec![XIEventMask::KEY_PRESS],
         };
         xi_select_events(&self.conn, self.root, &[mask]).unwrap();
+        self.get_kbd();
         self.conn.flush().unwrap();
+    }
+
+    fn get_kbd(&mut self) {
+        let count = self.kbd.max_key - self.kbd.first_key + 1;
+        let reply = self
+            .conn
+            .get_keyboard_mapping(self.kbd.first_key, count)
+            .unwrap()
+            .reply_unchecked()
+            .unwrap()
+            .unwrap();
+        self.kbd.keysyms = reply.keysyms;
+        self.kbd.keysyms_per = reply.keysyms_per_keycode;
     }
 
     pub fn start_input_listener() -> mpsc::Receiver<u32> {
